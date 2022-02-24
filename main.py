f16652127849b69d03f9565b9d4a8202b5e5a72a
Olya <haritonova.olyakpc2002020@gmail.com>
2022-02-24 17:59:39 +0300

    added function_a

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import os
 
+
+def userpath():
+    while True:
+        path1 = input("Введите путь до папки: ")
+        if os.path.exists(path1):
+            break
+        else:
+            print("Вы не правильно ввели путь")
+    return path1
+
+
 def get_file_sizes(folder_path):
-    result={}
-    for path,folders,files in os.walk(folder_path):
+    result = {}
+    for path, folders, files in os.walk(folder_path):
         for file in files:
-            file_path=os.path.join(path, file)
-            file_size=os.path.getsize(file_path)
-            result[file_path]=file_size
+            file_path = os.path.join(path, file)
+            file_size = os.path.getsize(file_path)
+            result[file_path] = file_size
     return result
+
 
 def get_duplicates(d):
     result = {}
@@ -19,8 +31,9 @@ def get_duplicates(d):
             result[(name, size)] = [path]
     return {k: v for k, v in result.items() if len(v) > 1}
 
+
 def get_results(d):
-    for k,v in d.items():
+    for k, v in d.items():
         print(k)
-        print('\n'.join(['\t'+ i for i in v]))
+        print('\n'.join(['\t' + i for i in v]))
         print()
